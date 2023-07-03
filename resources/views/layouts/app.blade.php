2ec8020286a9c903b7d8b0ee2627aab6fdb71465
Rhea Marstaller <r.marstaller@gmail.com>
2023-07-03 12:31:15 +0200

    Revert "feat: Prepare Filament table builder and form builder to be used in non admin area"
    This reverts commit b6cee19a03f30ac355b6234c0561cfe6d435fbed.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,14 +2,10 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-
-        <meta name="application-name" content="{{ config('app.name') }}">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
-
-        <style>[x-cloak] { display: none !important; }</style>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -17,9 +13,6 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-        @livewireStyles
-        @livewireScripts
-        @stack('scripts')
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
@@ -39,6 +32,5 @@
                 {{ $slot }}
             </main>
         </div>
-        @livewire('notifications')
     </body>
 </html>
