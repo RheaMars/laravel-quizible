176e3891b6aaac235134fa0d22ef9613dc4b3452
Michael Lackenberger <45495538+milack68@users.noreply.github.com>
2023-12-28 20:26:23 +0100

    WIP - Start working on feature learn FlashCards

--- app/Livewire/LearnFlashCards.php
+++ app/Livewire/LearnFlashCards.php
@@ -3,9 +3,46 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\Category;
+use App\Models\FlashCard;
+use Illuminate\Support\Facades\Auth;
+use App\Filament\Resources\FlashCardResource;
+
 
 class LearnFlashCards extends Component
 {
+    public $courses;
+    public $categories;
+    public $selectedCourse;
+    public $selectedCategory;
+    public $flashcards;
+
+    public function mount() {
+        $user = Auth::user();
+        $this->courses = $user->courses->all();
+        $this->categories = null;
+        $this->selectedCourse = null;
+        $this->selectedCategory = null;
+        $this->flashcards = null;
+    }
+
+    public function updatedSelectedCourse($course) {
+        if ($course === '') {
+            $this->selectedCourse = null;
+            $this->categories = null;
+        } else {
+            $this->categories = Category::where('course_id', $course)->get()->sortBy('name');
+        }
+    }
+
+    public function learnFlashCards() {
+        if($this->selectedCategory) {
+            $this->flashcards = FlashCard::where('category_id', $this->selectedCategory)->get();
+        } else {
+            $this->flashcards = FlashCard::where('course_id', $this->selectedCourse)->get();
+        }
+    }
+
     public function render()
     {
         return view('livewire.learn-flash-cards');
--- resources/views/livewire/learn-flash-cards.blade.php
+++ resources/views/livewire/learn-flash-cards.blade.php
@@ -1,4 +1,46 @@
 <div>
-    Hallo!
-    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
+    <x-filament::fieldset>
+        <div class="grid grid-cols-3 gap-4">
+            <div>
+                <label for="">Fach</label>
+                <x-filament::input.wrapper>
+                    <x-filament::input.select wire:model.live='selectedCourse'>
+                        <option value="" selected>Wählen Sie eine Option</option>
+                        @foreach ($courses as $course)
+                            <option value="{{ $course->id }}">{{ $course->name }}</option>
+                        @endforeach
+                    </x-filament::input.select>
+                </x-filament::input.wrapper>
+            </div>
+            <div>
+                @if ($categories != null)
+                    <label for="">Kategorie</label>
+                    <x-filament::input.wrapper>
+                        <x-filament::input.select wire:model.live='selectedCategory'>
+                            <option value="" selected>Alle Kategorien (komplettes Fach)</option>
+                            @foreach ($categories as $category)
+                                <option value="{{ $category->id }}">{{ $category->name }}</option>
+                            @endforeach
+                        </x-filament::input.select>
+                    </x-filament::input.wrapper>
+                @endif
+            </div>
+        </div>
+        <div class="py-4">
+            @if ($categories)
+                <x-filament::button wire:click='learnFlashCards'>
+                    Jetzt lernen
+                </x-filament::button>
+            @endif
+        </div>
+    </x-filament::fieldset>
+    @if($flashcards)
+        <x-filament::fieldset>
+            <ul>
+                @foreach($flashcards as $flashcard)
+                    <li>Flashcard-ID: {{ $flashcard->id }}</li>
+                @endforeach
+            </ul>
+        </x-filament::fieldset>
+    @endif
 </div>
